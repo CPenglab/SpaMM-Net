3ad856859716171a986573353cc6a467dbc75b95
zfy10086 <70880745+zfy10086@users.noreply.github.com>
2025-07-31 23:58:00 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,26 +1,16 @@
 import pandas as pd
 import scanpy as sc
+import seaborn as sns
 
 import spamm as spm
 
 # load data
-om1_adata, om2_adata = spm.load_test_data(idx = 0)
-
-# processing
-spm.preprocessing(
-    om1_adata, om2_adata,
-    n_comps = 50, random_state = 2025
-)
-adata = spm.adata_const(
-    om1_adata, om2_adata,
-    spatial_net_k = 8,
-    om_net_k = [8, 15]
-)
+adata = spm.load_test_data(idx = 0)
 
 # training
 output, model = spm.run_spamm(
     adata, scale = 3, epochs = 600, lr = 0.001, lr_step = [100, 200], gamma = 0.1,
-    rtn_model = True
+    rtn_model = True, acc_ctrl=False
 )
 
 # clustering
@@ -33,7 +23,7 @@ sc.pp.neighbors(
 )
 sc.tl.leiden(
     adata_feat,
-    resolution = 1.2,
+    resolution = 0.85,
     flavor="igraph",
     random_state=2025
 )
