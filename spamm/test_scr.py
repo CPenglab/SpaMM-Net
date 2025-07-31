29481fed16ebd58ba29b1ad0527a5cf5063aff97
zfy10086 <70880745+zfy10086@users.noreply.github.com>
2025-07-31 22:23:30 +0800

    Update test_scr.py

--- spamm/test_scr.py
+++ spamm/test_scr.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import scanpy as sc
-def load_test_data(idx):
-    ds_names = [ "H3K27me3","E18_5-S2", "ME13_50um"]
-    root = Path(__file__).parent.parent.absolute() / "datasets/"
-    data_path = root / ds_names[idx]
-    om1_adata = sc.read_h5ad(data_path / "rna_adata.h5ad")
-    om2_adata = sc.read_h5ad(data_path / "peak_adata.h5ad")
-    return om1_adata, om2_adata
+def load_test_data(idx = 0):
+    ds_names = ["H3K27me3", "E18_5-S2", "ME13_50um"]
+    root = Path(__file__).parent.parent.absolute()
+    data_path = root / "datasets"
+    adata = sc.read_h5ad(data_path / f"{ds_names[idx]}.h5ad")
+    print(f"Loaded {ds_names[idx]}.h5ad successfully.")
+    return adata
