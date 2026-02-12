e21c2f962d1be3108d47713409bcd0e6dc498ccd
zfy10086 <70880745+zfy10086@users.noreply.github.com>
2026-02-12 22:06:05 +0800

    Update test_scr.py

--- spamm/test_scr.py
+++ spamm/test_scr.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import scanpy as sc
 def load_test_data(idx = 0):
-    ds_names = ["H3K27me3", "E18_5-S2", "ME13_50um"]
+    ds_names = ["sc_multi_sim", "H3K27me3", "E18_5-S2", "ME13_50um"]
     root = Path(__file__).parent.parent.absolute()
     data_path = root / "datasets"
     adata = sc.read_h5ad(data_path / f"{ds_names[idx]}.h5ad")
